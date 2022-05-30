82644eae8382e82bd65dea7011899bcf409b1091
franioli <francesco.ioli@polimi.it>
2022-05-30 16:19:05 +0200

    init repo

---
+++ __main__.py
@@ -0,0 +1,20 @@
+# Belvedere stereo matching
+# 
+# 
+# 
+# 
+# 
+# v0.1 2022.05.17
+
+
+from pathlib import Path
+import argparse
+import random
+import numpy as np
+import matplotlib.cm as cm
+import  pydegensac
+from copy import deepcopy
+import cv2 
+
+if __name__ == '__main__':
+   
---
+++ belv_utiles.py
@@ -0,0 +1,65 @@
+"""
+
+"""
+
+from pathlib import Path
+import argparse
+import random
+import numpy as np
+import  pydegensac
+from copy import deepcopy
+import cv2 
+import matplotlib.pyplot as plt
+import os, glob
+
+
+class Camera:
+    """ Class to help manage Cameras. """
+    
+    """Initialise the camera calibration object 
+    Parameters
+    ----------
+    *args : str
+      Read calibration text file, with Full OpenCV parameters in a row.
+    """
+
+    def __init__(self, *args):
+        
+        self.reset()
+        if args[0] == None:
+            print('No calibration file available. Setting default parameters')
+        self.K = np.array([[6620.56653699, 0., 3020.0385], [0., 6619.88882, 1886.01352], [0., 0., 1.]] )
+        
+    
+    def reset(self):
+        self.K = np.array([[500, 0., 500], [0., 500, 500], [0., 0., 1.]] )
+        self.R = np.identity(3)
+        self.t = np.zeros((3,))
+        self.P = cv2.projectionFromKRt(self.K, self.R, self.t)
+        
+               
+cv2.projectionFromKRt(K, R, t)
+
+class images:
+    """ Class to help manage Cameras. """
+    
+    """Initialise the camera calibration object 
+    Parameters
+    ----------
+    *args : str
+      Read calibration text file, with Full OpenCV parameters in a row.
+    """
+
+    def __init__(self, *args):
+        
+        self.reset()
+        if args[0] == None:
+            print('No calibration file available. Setting default parameters')
+        self.K = np.array([[6620.56653699, 0., 3020.0385], [0., 6619.88882, 1886.01352], [0., 0., 1.]] )
+        
+    
+    def reset(self):
+        self.K = np.array([[500, 0., 500], [0., 500, 500], [0., 0., 1.]] )
+
+        
+               
