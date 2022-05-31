5701c71073d8afc67c4273c44726b519bd6b5055
franioli <francesco.ioli@polimi.it>
2022-05-31 21:20:07 +0200

    update

--- __main__.py
+++ __main__.py
@@ -6,15 +6,5 @@
 # 
 # v0.1 2022.05.17
 
-
-from pathlib import Path
-import argparse
-import random
-import numpy as np
-import matplotlib.cm as cm
-import  pydegensac
-from copy import deepcopy
-import cv2 
-
 if __name__ == '__main__':
    
