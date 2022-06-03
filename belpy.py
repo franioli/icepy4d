5b095632000494c856dcd9215d434c3d4d98ca30
franioli <francesco.ioli@polimi.it>
2022-06-03 17:27:37 +0200

    sfm by\ hloc and colmap
    Former-commit-id: dc997028993a18e2bcf1598760ce5796ce58096b

--- belpy.py
+++ belpy.py
@@ -208,3 +208,39 @@ for epoch in epoches2process:
 # with open(pth, 'rb') as f:
 #     a = pickle.load(f)
 
+from utils.geometry import (estimate_pose)
+
+
+kpts0, kpts1 = features[0]['mkpts0'], features[0]['mkpts1']
+K0, K1 = cameras[0]['K'], cameras[1]['K']
+thresh = 3
+conf=0.99999
+
+f_mean = np.mean([K0[0, 0], K1[1, 1], K0[0, 0], K1[1, 1]])
+norm_thresh = thresh / f_mean
+
+kpts0 = (kpts0 - K0[[0, 1], [2, 2]][None]) / K0[[0, 1], [0, 1]][None]
+kpts1 = (kpts1 - K1[[0, 1], [2, 2]][None]) / K1[[0, 1], [0, 1]][None]
+
+E, mask = cv2.findEssentialMat(
+    kpts0, kpts1, np.eye(3), threshold=norm_thresh, prob=conf,
+    method=cv2.RANSAC)
+
+assert E is not None
+
+best_num_inliers = 0
+ret = None
+for _E in np.split(E, len(E) / 3):
+    n, R, t, _ = cv2.recoverPose(
+        _E, kpts0, kpts1, np.eye(3), 1e9, mask=mask)
+    if n > best_num_inliers:
+        best_num_inliers = n
+        ret = (R, t[:, 0], mask.ravel() > 0)
+
+scaleFct =  259.9348
+
+P = np.dot(K0, np.concatenate((R, t), axis=1))
+X0 = - P(:,1:3) 
+
+
+
