2dc2be14b8bb5ce648490d47a0e3b5569a7076d2
franioli <francesco.ioli@polimi.it>
2022-06-16 08:44:55 +0200

    implementing point color interpolation

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,70 @@
 import cv2
 import numpy as np
+import scipy as sp
 
 def normalize_and_und_points(pts, K, dist=None):
     pts = cv2.undistortPoints(pts.T, K, dist)
     return pts
+
+def undistort_image(image, K, dist, downsample=1, out_path=None):
+    '''
+    Undistort image with OpenCV
+    '''
+    h, w, _ = image.shape
+    h_new, w_new = h*downsample, w*downsample
+    K_scaled, roi = cv2.getOptimalNewCameraMatrix(K, dist, (w, h), 1, (int(w_new), int(h_new)))
+    und = cv2.undistort(image, K, dist, None, K_scaled)
+    x, y, w, h = roi
+    und = und[y:y+h, x:x+w]  
+    if out_path is not None:
+        cv2.imwrite(out_name, und)
+    return und, K_scaled
+
+def interpolate_point_colors(pointsXYZ, image, K, R, t, dist=None, winsz=1, do_viz=False):
+    ''''
+    Interpolate color of a 3D sparse point cloud, given an oriented image
+      Inputs:  
+       - 3xN matrix with 3d world points coordinates
+       - image
+       - camera interior and exterior orientation matrixes: K, R, t
+       - distortion vector according to OpenCV
+    Output: 3xN colour matrix 
+    '''
+    
+    assert K is not None, 'invalid camera matrix' 
+    assert R is not None, 'invalid rotation matrix' 
+    assert t is not None, 'invalid translation vector' 
+
+    if K is not None and dist is not None:
+        image = undistort_image(image, K, dist)
+    
+    numPts = len(pointsXYZ)
+    col = np.zeros((3,numPts))
+    h,w = image.shape
+    m,_ = cv2.projectPoints(pointsXYZ, cv2.Rodrigues(R), t, K, dist)
+
+    for k in range(0,numPts):
+        kint = np.round(m[:,k])
+        i = [a for a in range(kint[1]-winsz,kint[1]+winsz)]
+        j = [b for b in range(kint[0]-winsz,kint[0]+winsz)]
+        if i > w or j > h:
+            continue
+        ii, jj = np.meshgrid(i,j)
+        for rgb in range(0,3):
+            colNum  = image[i,j,rgb].astype(float64).flatten
+
+#         for  rgb = 1:3
+#             colNum  = double(im(i(:),j(:),rgb));
+#             fcol    = scatteredInterpolant(jj(:),ii(:),colNum(:), 'linear');
+#             col(rgb,k) = fcol(m(1,k), m(2,k));
+   
+#     col = uint8(col);
+
+#     if do_viz == true
+#         figure; imshow(im); hold on;  axis on; 
+#         scatter(m(1,:), m(2,:), 20, double(col(:, :))'/255,  'filled' , 'MarkerEdgeColor', 	[0 0 0])
+
+    
 
 
 ## Visualization
