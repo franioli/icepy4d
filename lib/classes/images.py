449160969de02fc7e929758b03787c54e69ff559
franioli <francesco.ioli@polimi.it>
2022-06-24 21:50:02 +0200

    added classes files

---
+++ lib/classes/images.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class Features:
+    def __init__(self):
+        print()
+        
+# class images:
+#     """ Class to help manage Cameras. """
+    
+#     """Initialise the camera calibration object 
+#     Parameters
+#     ----------
+#     *args : str
+#       Read calibration text file, with Full OpenCV parameters in a row.
+#     """
+
+#     def __init__(self, *args):
+        
+#         self.reset()
+
+        
+#     def reset(self):
