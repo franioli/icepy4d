9e7edc7a9e7f23cb656864abd3b9926e0d7b0446
franioli <francesco.ioli@polimi.it>
2022-07-06 18:56:41 +0200

    cleaned code
    Former-commit-id: 3dc33c30037407f0ee3cbf09eb31979b4f46f712

--- belpy_main.py
+++ belpy_main.py
@@ -360,28 +360,6 @@ o3d.visualization.draw_geometries(pcd, window_name='All epoches',
                                     width=1280, height=720, 
                                     left=300, top=200)
 
-# %% Plot targets
-# #  Plot with matplotlib
-# cam = 1
-# i = 0
-# _plot_style = dict(markersize=5, markeredgewidth=2,
-#                     markerfacecolor='none', markeredgecolor='r',
-#                     marker='x', linestyle='none')
-
-# xy = targets.get_im_coord(cam)[i]
-# img = cv2.cvtColor(images[cam][i], cv2.COLOR_BGR2RGB)
-# fig, ax = plt.subplots()
-# ax.imshow(img) 
-# ax.plot(xy[0], xy[1], **_plot_style)
-    
-# %% Save reconstruction
-# with h5py.File("test.hdf5", "w") as f:
-#     dset = f.create_dataset("features", data=features)
-    
-    
-# dset = h5py.File("test.hdf5", "r")
-# dset.close()
-
 #%% DSM 
 res = 0.03
 dsms = []
