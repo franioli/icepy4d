791215963fa7cb667d724a1de4916dc8df311a13
franioli <francesco.ioli@polimi.it>
2022-10-27 10:05:46 +0200

    least squares for Absolute Orientation
    intagrated functions for least squares estimation using lmfit in Absolute Orientation Class. It can be chosen instead of linear estimation.
    
    
    Former-commit-id: 07a260279e0b1decc7bbb3c21b18dee54ba797b9 [formerly e8350bbb017665aa3167f5a18fc931881d531d98]
    Former-commit-id: e2db1b6f9ca47799dea2e0c4b4e9658da88e2e57
    Former-commit-id: 7cd85e5ac62231d9dd4df145e063e305cce377f0
    Former-commit-id: 194059a093846e1bfd3f65772d2811c0f41c4039
    Former-commit-id: 1baceb14a2b5bc8f4dc9f79ea7f237438f8d1e97

--- lib/sfm/absolute_orientation.py
+++ lib/sfm/absolute_orientation.py
@@ -2,13 +2,18 @@ from ast import Assert
 import numpy as np
 import cv2
 from typing import List, Tuple
+from lmfit import Minimizer, Parameters, fit_report
+from scipy import stats
 
 from lib.classes import Camera
 from lib.sfm.triangulation import Triangulate
 from lib.misc import convert_to_homogeneous, convert_from_homogeneous
 
-
-from thirdparty.transformations import affine_matrix_from_points
+from thirdparty.transformations import (
+    affine_matrix_from_points,
+    euler_from_matrix,
+    euler_matrix,
+)
 
 
 ''' Space resection class for orienting one single image in world space'''
@@ -43,6 +48,149 @@ class Space_resection():
 
 
 ''' Absolute orientation class for georeferencing model'''
+
+
+def get_T_from_params(
+    params: Parameters,
+):
+
+    # Get parameters
+    parvals = params.valuesdict()
+    rx = parvals['rx']
+    ry = parvals['ry']
+    rz = parvals['rz']
+    tx = parvals['tx']
+    ty = parvals['ty']
+    tz = parvals['tz']
+    m = parvals['m']
+
+    # Build 4x4 transformation matrix (T) in homogeneous coordinates
+    T = np.identity(4)
+    R = euler_matrix(rx, ry, rz)
+    T[0:3, 0:3] = (m * np.identity(3)) @ R[:3, :3]
+    T[0:3, 3:4] = np.array([tx, ty, tz]).reshape(3, 1)
+
+    return T
+
+
+def compute_residuals(
+    params: Parameters,
+    x0: np.ndarray,
+    x1: np.ndarray,
+    weights: np.ndarray = None,
+    prior_covariance_scale: float = None,
+) -> np.ndarray:
+    ''' 3D rototranslation with scale factor
+
+    X1_ = T_ + m * R * X0_
+
+    Inputs: 
+    - x0 (np.ndarray): Points in the starting reference system
+    - x1 (np.ndarray): Points in final reference system 
+    - weights (np.ndarray, defult = None): weights (e.g., inverse of a-priori observation uncertainty)
+    - prior_covariance_scale (float, default = None): A-priori sigma_0^2     
+
+    Return: 
+    - res (nx1 np.ndarray): Vector of the weighted residuals
+
+    '''
+
+    # Get parameters from params
+    T = get_T_from_params(params)
+
+    # Convert points to homogeneos coordinates and traspose np array to obtain a 4xn matrix
+    # Note that here the function convert_to_homogeneous is different than that implemented in rotra3d repo and it requires the dimensions to be along the colums direction.
+    x0 = convert_to_homogeneous(x0.T)
+
+    # Apply transformation to x0 points
+    x1_ = T @ x0
+    x1_ = x1_[:3, :].T
+
+    # Compute residuals as differences between observed and estimated values, scaled by the a-priori observation uncertainties
+    res = (x1 - x1_)
+
+    # If weigthts are provided, scale residual
+    if weights is not None:
+
+        if weights.shape != res.shape:
+            raise ValueError(
+                f'Wrong dimensions of the weight matrix. It must be of size {res.shape}')
+
+        res = res * weights
+
+    return res.flatten()
+
+
+def rescale_residuals(residuals: np.ndarray,
+                      weights: np.ndarray,
+                      ) -> np.ndarray:
+
+    residuals = residuals / weights
+    return residuals
+
+
+def print_results(
+    result,
+    weights: np.ndarray = None,
+    sigma0_2: float = 1.,
+) -> None:
+
+    ndim = weights.shape[1]
+
+    # Rescale residuals
+    if weights is not None:
+        residuals = rescale_residuals(
+            result.residual.reshape(-1, ndim),
+            weights,
+        )
+    else:
+        residuals = result.residual.reshape(-1, ndim)
+
+    print('-------------------------------')
+    print('Optimization report')
+    print(fit_report(result))
+
+    print('-------------------------------')
+    print(f'Chi quadro test:')
+    nfree = result.nfree
+    chi_lim = stats.chi2.ppf(1-.05, df=nfree)
+    chi_0 = result.redchi / sigma0_2
+    print(f'Degrees of freedom: {nfree}')
+    print(f'Chi2 empirical: {chi_0:.3f}')
+    print(f'Chi2 limit: {chi_lim:.3f}')
+    if chi_0 < chi_lim:
+        print('Test passed')
+    else:
+        print('Test NOT passed')
+
+    print('-------------------------------')
+    print('Residuals')
+    # print('     X       Y      Z')
+    # print(f'{res[0]:8.3f} {res[1]:8.3f} {res[2]:8.3f}')
+    for res in residuals:
+        for dim in range(ndim):
+            if dim == ndim-1:
+                endline = '\n'
+            else:
+                endline = ' '
+            print(f"{res[dim]:8.3f}", end=endline)
+
+    print('-------------------------------')
+    print(f'Covariance matrix:')
+    for var in result.var_names:
+        if var is result.var_names[-1]:
+            endline = '\n'
+        else:
+            endline = ' '
+        print(f"   {var:7s}", end=endline)
+
+    for row in result.covar:
+        for cov in row:
+            if cov == row[-1]:
+                endline = '\n'
+            else:
+                endline = ' '
+            print(f"{cov:10.5f}", end=endline)
 
 
 class Absolute_orientation():
@@ -122,7 +270,7 @@ class Absolute_orientation():
         return triangulation.points3d
 
     def estimate_transformation_linear(self,
-                                       estimate_scale:  bool = False,
+                                       estimate_scale:  bool = True,
                                        ) -> np.ndarray:
         ''' Wrapper around 'affine_matrix_from_points' function from 'transformation'. It estimates 3D rototranslation by using SVD
         '''
@@ -138,8 +286,82 @@ class Absolute_orientation():
 
         return self.tform
 
-    def estimate_transformation_least_squares(self):
-        pass
+    def extract_params_from_T(self, T: np.ndarray = None):
+        ''' Extract transformation parameters from 4x4 T matrix
+        --------------
+        Parameters:
+            - T (4x4 np.ndarray = None): 4x4 transformation matrix. If not provided, self.tform is used.
+        Returns: params (dict): transformation parameters.
+        --------------
+        '''
+
+        if T is None:
+            T = self.tform
+
+        t = T[:3, 3:4].squeeze()
+        rot = euler_from_matrix(T[:3, :3])
+        m = float(1.0)
+        prm = {
+            'rx': rot[0],
+            'ry': rot[1],
+            'rz': rot[2],
+            'tx': t[0],
+            'ty': t[1],
+            'tz': t[2],
+            'm': m,
+        }
+
+        return prm
+
+    def estimate_transformation_least_squares(self,
+                                              uncertainty: np.ndarray = None,
+                                              ) -> np.ndarray:
+        ''' Estimate 3D rototranslation with least squares, by using lmfit library.
+        --------------
+        Parameters: 
+            - uncertainty (np.array): numpy array of the same dimension of self.v0 and self.v1 matrixes used to scale the residuals. The uncertainty matrix contains the a priori standard deviation of each observation and it is analogous as building a diagonal Q matrix with the variance of the observations along the main diagonal.
+        Returns: self.tform (4x4 np.ndarray): Transformation matrix.
+        --------------
+        '''
+        # @TODO: storing and returning also covariance matrix and other useful information.
+
+        # Estimate approximate values by using estimate_transformation_linear
+        T = self.estimate_transformation_linear()
+        prm = self.extract_params_from_T(T)
+
+        # Define Parameters to be optimized
+        params = Parameters()
+        params.add('rx', value=prm['rx'], vary=True)
+        params.add('ry', value=prm['ry'], vary=True)
+        params.add('rz', value=prm['rz'], vary=True)
+        params.add('tx', value=prm['tx'], vary=True)
+        params.add('ty', value=prm['ty'], vary=True)
+        params.add('tz', value=prm['tz'], vary=True)
+        params.add('m',  value=prm['m'], vary=True)
+
+        if uncertainty is None:
+            # Default assigned uncertainty
+            uncertainty = np.ones(self.v0.shape)
+
+        # Run Optimization!
+        weights = 1. / uncertainty
+        minimizer = Minimizer(
+            compute_residuals,
+            params,
+            fcn_args=(
+                self.v0,
+                self.v1,
+            ),
+            fcn_kws={
+                'weights': weights,
+            },
+            scale_covar=True,
+        )
+        ls_result = minimizer.minimize(method='leastsq')
+        # fit_report(result)
+
+        # Print result
+        print_results(ls_result, weights)
 
     def apply_transformation(self,
                              T: np.ndarray = None,
@@ -149,9 +371,9 @@ class Absolute_orientation():
         ''' Apply estimated transformation to 3D points and to camera matrices
         --------------
         Parameters:
-            - T (4x4 np.ndarray = None): 4x4 transformation matrix. If provided, they overwrite the self.tform.
-            - points3d (nx3 np.ndarray = None): Coordinates of the points to be transformed. If provided, they overwrite the self.v1 points.
-            - camera (Camera, default=None): Camera object. If provided, it overwrites the old point stored in the class object.
+            - T (4x4 np.ndarray = None): 4x4 transformation matrix. If not provided, self.tform is used.
+            - points3d (nx3 np.ndarray = None): Coordinates of the points to be transformed.If not provided, self.v1 is used.
+            - camera (Camera, default=None): Camera object. If not provided, self.cameras are used.
         Returns: self.v1 (nx3 np.ndarray): transformed points.
         --------------
         '''
