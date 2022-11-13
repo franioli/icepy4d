ae03cd10ea27cd02ae11b0e5cc58499d1cd32518
franioli <francesco.ioli@polimi.it>
2022-11-13 20:17:43 +0100

    moved present matching and tracking fct to matching_base.py
    Former-commit-id: 71eb11402e30cb1e7f805dc599c1c3fd2d5ed57b [formerly e77f6aa073875d404eff85223e76a7661b48d8d4]
    Former-commit-id: 27434c57a14131af6801208f061b1abc64689111
    Former-commit-id: 4290b467368f89f70a41cd65efba41deff1b9c74
    Former-commit-id: b4adc7fdecf96dcb92472e866750be4707575da2
    Former-commit-id: f25210a3cba54da30bd71a9e1c234bac1861a7af

--- belpy.py
+++ belpy.py
@@ -1,19 +1,12 @@
 import numpy as np
 import cv2
 import pickle
-import json
-import pydegensac
 
-from pathlib import Path
-from easydict import EasyDict as edict
 from copy import deepcopy
 from shutil import copy as scopy
 
 from lib.classes import Camera, Imageds, Features, Targets
-
-from lib.matching.match_pairs import match_pair
-from lib.matching.track_matches import track_matches
-
+from lib.matching.matching_base import MatchingTracking
 from lib.sfm.two_view_geometry import Two_view_geometry
 from lib.sfm.triangulation import Triangulate
 from lib.sfm.absolute_orientation import (
@@ -21,19 +14,17 @@ from lib.sfm.absolute_orientation import (
     Space_resection,
 )
 
-from lib.utils import (
-    build_dsm,
-    generate_ortophoto,
-)
-
+from lib.config import parse_yaml_cfg, validate_inputs
 from lib.point_clouds import (
     create_point_cloud,
     write_ply,
 )
-
+from lib.utils import (
+    create_directory,
+    build_dsm,
+    generate_ortophoto,
+)
 from lib.visualization import display_point_cloud
-from lib.utils import create_directory
-from lib.config import parse_yaml_cfg, validate_inputs
 
 from thirdparty.transformations import euler_from_matrix, euler_matrix
 
@@ -50,117 +41,16 @@ features = dict.fromkeys(cams)  # @TODO: put this in an inizialization function
 images = dict.fromkeys(cams)  # @TODO: put this in an inizialization function
 for cam in cams:
     images[cam] = Imageds(cfg.paths.imdir / cam)
-
+    
 cfg = validate_inputs(cfg, images)
 
 """ Perform matching and tracking """
-# Load matching and tracking configurations
-with open(cfg.matching_cfg) as f:
-    opt_matching = edict(json.load(f))
-with open(cfg.tracking_cfg) as f:
-    opt_tracking = edict(json.load(f))
-
-# epoch = 0
 if cfg.proc.do_matching:
-    for cam in cams:
-        features[cam] = []
-
-    for epoch in cfg.proc.epoch_to_process:
-        print(f"Processing epoch {epoch}...")
-
-        # opt_matching = cfg.matching.copy()
-        epochdir = Path(cfg.paths.resdir) / f"epoch_{epoch}"
-
-        # -- Find Matches at current epoch --#
-        print(f"Run Superglue to find matches at epoch {epoch}")
-        opt_matching.output_dir = epochdir
-        pair = [
-            images[cams[0]].get_image_path(epoch),
-            images[cams[1]].get_image_path(epoch),
-        ]
-        # Call matching function
-        matchedPts, matchedDescriptors, matchedPtsScores = match_pair(
-            pair, cfg.images.bbox, opt_matching
-        )
-
-        # Store matches in features structure
-        for jj, cam in enumerate(cams):
-            # Dict keys are the cameras names, internal list contain epoches
-            features[cam].append(Features())
-            features[cam][epoch].append_features(
-                {
-                    "kpts": matchedPts[jj],
-                    "descr": matchedDescriptors[jj],
-                    "score": matchedPtsScores[jj],
-                }
-            )
-            # @TODO: Store match confidence!
-
-        # === Track previous matches at current epoch ===#
-        if cfg.proc.do_tracking and epoch > 0:
-            print(f"Track points from epoch {epoch-1} to epoch {epoch}")
-
-            trackoutdir = epochdir / f"from_t{epoch-1}"
-            opt_tracking["output_dir"] = trackoutdir
-            pairs = [
-                [
-                    images[cams[0]].get_image_path(epoch - 1),
-                    images[cams[0]].get_image_path(epoch),
-                ],
-                [
-                    images[cams[1]].get_image_path(epoch - 1),
-                    images[cams[1]].get_image_path(epoch),
-                ],
-            ]
-            prevs = [
-                features[cams[0]][epoch - 1].get_features_as_dict(),
-                features[cams[1]][epoch - 1].get_features_as_dict(),
-            ]
-            # Call actual tracking function
-            tracked_cam0, tracked_cam1 = track_matches(
-                pairs, cfg.images.bbox, prevs, opt_tracking
-            )
-            # @TODO: keep track of the epoch in which feature is matched
-            # @TODO: Check bounding box in tracking
-            # @TODO: clean tracking code
-
-            # Store all matches in features structure
-            features[cams[0]][epoch].append_features(tracked_cam0)
-            features[cams[1]][epoch].append_features(tracked_cam1)
-
-        # Run Pydegensac to estimate F matrix and reject outliers
-        F, inlMask = pydegensac.findFundamentalMatrix(
-            features[cams[0]][epoch].get_keypoints(),
-            features[cams[1]][epoch].get_keypoints(),
-            px_th=1.5,
-            conf=0.99999,
-            max_iters=10000,
-            laf_consistensy_coef=-1.0,
-            error_type="sampson",
-            symmetric_error_check=True,
-            enable_degeneracy_check=True,
-        )
-        print(
-            f"Matching at epoch {epoch}: pydegensac found {inlMask.sum()} \
-            inliers ({inlMask.sum()*100/len(features[cams[0]][epoch]):.2f}%)"
-        )
-        features[cams[0]][epoch].remove_outliers_features(inlMask)
-        features[cams[1]][epoch].remove_outliers_features(inlMask)
-
-        # Write matched points to disk
-        im_stems = images[cams[0]].get_image_stem(epoch), images[
-            cams[1]
-        ].get_image_stem(epoch)
-        for jj, cam in enumerate(cams):
-            features[cam][epoch].save_as_txt(
-                epochdir / f"{im_stems[jj]}_mktps.txt")
-        with open(epochdir / f"{im_stems[0]}_{im_stems[1]}_features.pickle", "wb") as f:
-            pickle.dump(features, f, protocol=pickle.HIGHEST_PROTOCOL)
-        last_match_path = create_directory("res/last_epoch")
-        with open(last_match_path / "last_features.pickle", "wb") as f:
-            pickle.dump(features, f, protocol=pickle.HIGHEST_PROTOCOL)
-
-        print("Matching completed")
+    features = MatchingAndTracking(
+        cfg=cfg,
+        images=images,
+        features=features,
+    )
 
 elif not features[cams[0]]:
     last_match_path = "res/last_epoch/last_features.pickle"
@@ -168,7 +58,7 @@ elif not features[cams[0]]:
         features = pickle.load(f)
         print("Loaded previous matches")
 else:
-    print("Features already present, nothing was changed.")
+    print("Features already present.")
 
 
 """ SfM """
