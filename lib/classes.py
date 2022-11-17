88c064602bf5d34d1d431c079c11a65e9b96c255
Francesco Ioli <francesco.ioli@polimi.it>
2022-11-17 12:11:53 +0100

    updated old camera class to CameraNew class in all the scripts

--- lib/classes.py
+++ lib/classes.py
@@ -43,38 +43,43 @@ class CameraNew:
 
     def __init__(
         self,
-        width=None,
-        height=None,
-        K=None,
-        dist=None,
-        R=None,
-        t=None,
-        calib_path=None,
+        width: np.ndarray,
+        height: np.ndarray,
+        K: np.ndarray = None,
+        dist: np.ndarray = None,
+        R: np.ndarray = None,
+        t: np.ndarray = None,
+        extrinsics: np.ndarray = None,
+        calib_path: Union[str, Path] = None,
     ):
         """Initialize pinhole camera model"""
-        # TODO: add checks on inputs
-        # If not None, convert inputs to np array
-        if K is not None:
-            K = np.array(K)
-        if R is not None:
-            R = np.array(R)
-        if t is not None:
-            t = np.array(t)
-        if dist is not None:
-            dist = np.array(dist)
-        # TODO: add assertion to check that only K and dist OR calib_path is provided.
-
         self._w = width  # Image width [px]
         self._h = height  # Image height [px]
         self._K = K  # Calibration matrix (Intrisics)
         self._dist = dist  # Distortion vector in OpenCV format
         self.reset_EO()
 
+        if R is not None and t is not None:
+            self._extrinsics = self.Rt_to_extrinsics(R, t)
+
+        if extrinsics is not None:
+            self._extrinsics = extrinsics
+
         # If calib_path is provided, read camera calibration from file
         if calib_path is not None:
             self.read_calibration_from_file(calib_path)
 
     # Getters
+    @property
+    def width(self) -> np.ndarray:
+        """Get image width"""
+        return self._w
+
+    @property
+    def height(self) -> np.ndarray:
+        """Get image height"""
+        return self._h
+
     @property
     def K(self) -> np.ndarray:
         """Get Intrinsics matrix"""
@@ -87,8 +92,13 @@ class CameraNew:
 
     @property
     def extrinsics(self) -> np.ndarray:
-        """Get extrinsics matrix"""
+        """Get Extrinsics Matrix (i.e., transformation from world to camera)"""
         return self._extrinsics
+
+    @property
+    def pose(self) -> np.ndarray:
+        """Get Pose Matrix (i.e., transformation from camera to world)"""
+        return self.extrinsics_to_pose()
 
     @property
     def C(self) -> np.ndarray:
@@ -107,19 +117,40 @@ class CameraNew:
         return self._extrinsics[0:3, 0:3]
 
     @property
+    def euler_angles(self) -> Tuple[float]:
+        return self.euler_from_R(self.R)
+
+    @property
     def P(self) -> np.ndarray:
         """Get Projective matrix P = K [ R | T ]"""
-        return self.K @ self._extrinsics
+        return self.K @ self._extrinsics[0:3, :]
 
+    # Setters
+    def update_K(self, K: np.ndarray) -> None:
+        self._K = K
+
+    def update_dist(self, dist: np.ndarray) -> None:
+        self._dist = dist
+
+    def update_extrinsics(self, extrinsics: np.ndarray) -> None:
+        """Update Camera Exterior Orientataion given a new extrinsics matrix.
+        Note that, for code safety, this is the only method to update the camera exterior Orientation. If you need to update the camera EO from a pose matrix or from R,t, compute the extrinsics matrix first with the method self.pose_to_extrinsics(pose) or Rt_to_extrinsics(R,t)
+        """
+        self._extrinsics = extrinsics
+
+    # Methods
     def reset_EO(self):
         """Reset camera External Orientation (EO), in such a way as to make camera reference system parallel to world reference system"""
         self._extrinsics = np.eye(4)
 
-    def extrinsics_to_pose(self) -> np.ndarray:
+    def extrinsics_to_pose(self, extrinsics: np.ndarray = None) -> np.ndarray:
         """Compute Pose matrix (i.e., transformation from camera to world) from extrinsics matrix (i.e, transformation from world to camera)"""
 
-        R = self._extrinsics[0:3, 0:3]
-        t = self._extrinsics[0:3, 3:4]
+        if extrinsics is None:
+            extrinsics = self._extrinsics
+
+        R = extrinsics[0:3, 0:3]
+        t = extrinsics[0:3, 3:4]
         Rc = R.T
         C = -np.dot(Rc, t)
         Rc_block = self.build_block_matrix(Rc)
@@ -127,7 +158,7 @@ class CameraNew:
 
         return np.dot(C_block, Rc_block)
 
-    def pose_to_extrinsics(self, pose):
+    def pose_to_extrinsics(self, pose: np.ndarray):
         """Returns Pose matrix given an extrinsics matrix"""
 
         Rc = pose[0:3, 0:3]
@@ -156,7 +187,7 @@ class CameraNew:
 
         return K, R, t
 
-    def Rt_to_extrinsics(self, R, t) -> np.ndarray:
+    def Rt_to_extrinsics(self, R: np.ndarray, t: np.ndarray) -> np.ndarray:
         """
         Return 4x4 Extrinsics matrix, given a 3x3 Rotation matrix and 3x1 translation vector, as follows:
 
@@ -164,40 +195,23 @@ class CameraNew:
         | --|-- |  = | --|-- | * | --|-- |
         [ 0 | 1 ]    [ 0 | 1 ]   [ 0 | 1 ]
         """
+        if len(t.shape) == 1 or t.shape == (1, 3):
+            assert t.shape[0] == 3, "Invalid translation vector"
+            t = t.reshape(3, 1)
         R_block = self.build_block_matrix(R)
         t_block = self.build_block_matrix(t)
         return t_block @ R_block
 
-    def C_from_P(self, P) -> np.ndarray:
+    def C_from_P(self, P: np.ndarray) -> np.ndarray:
         """
         turn the camera center from a projection matrix P, as
         C = [- inv(KR) * Kt] = [-inv(P[1:3]) * P[4]]
         """
-
         return -np.dot(np.linalg.inv(P[:, 0:3]), P[:, 3].reshape(3, 1))
 
-    """ Old methods"""
-
-    def build_camera_EO(
-        self, extrinsics: np.ndarray = None, pose: np.ndarray = None
-    ) -> None:
-
-        if extrinsics is not None:
-            self.extrinsics = extrinsics
-            self.extrinsics_to_pose()
-            self.update_camera_from_extrinsics()
-
-        elif pose is not None:
-            self.pose = pose
-            self.pose_to_extrinsics()
-            self.update_camera_from_extrinsics()
-
-        else:
-            raise ValueError(
-                "Not enough data to build Camera External Orientation matrixes."
-            )
-
     def build_pose_matrix(self, R: np.ndarray, C: np.ndarray) -> None:
+        """Return the"""
+
         # Check for input dimensions
         if R.shape != (3, 3):
             raise ValueError(
@@ -210,95 +224,45 @@ class CameraNew:
                 "Wrong dimension of the C vector. It must be a 3x1 or a 1x3 numpy array"
             )
 
-        self.pose = np.eye(4)
-        self.pose[0:3, 0:3] = R
+        pose = np.eye(4)
+        pose[0:3, 0:3] = R
         self.pose[0:3, 3:4] = C
 
-    def update_camera_from_extrinsics(self):
-        """ """
-        if self.extrinsics is None:
-            print("Camera extrinsics not available. Compute it first.")
-            return None
+    def euler_from_R(self, R):
+        """
+        Compute Euler angles from rotation matrix
+        - ------
+        Returns:  [omega, phi, kappa]
+        """
+        omega = np.arctan2(R[2, 1], R[2, 2])
+        phi = np.arctan2(-R[2, 0], np.sqrt(R[2, 1] ** 2 + R[2, 2] ** 2))
+        kappa = np.arctan2(R[1, 0], R[0, 0])
+
+        return (omega, phi, kappa)
+
+    def build_block_matrix(self, mat):
+        if mat.shape[1] == 3:
+            block = np.block([[mat, np.zeros((3, 1))], [np.zeros((1, 3)), 1]])
+        elif mat.shape[1] == 1:
+            block = np.block([[np.eye(3), mat], [np.zeros((1, 3)), 1]])
         else:
-            self.R = self.extrinsics[0:3, 0:3]
-            self.t = self.extrinsics[0:3, 3:4]
-            self.P = np.dot(self.K, self.extrinsics[0:3, :])
-            self.C_from_P()
-
-    def get_C_from_pose(self):
-        """ """
-        return self.pose[0:3, 3:4]
-
-    def t_from_RC(self):
-        """Deprecrated function. Use extrinsics_to_pose instead.
-        Compute and return the camera translation vector t, given the camera
-        centre and the roation matrix X, as
-        t = [-R * C]
-        The relation is derived from the formula of the camera centre
-        C = [- inv(KR) * Kt]
-        """
-        self.t = -np.dot(self.R, self.C)
-        self.compose_P()
-        return self.t
-
-    def compose_P(self):
-        """
-        Compose and return the 4x3 P matrix from 3x3 K matrix, 3x3 R matrix and 3x1 t vector, as:
-            K[R | t]
-        """
-        if self.K is None:
-            print("Invalid calibration matrix. Unable to compute P.")
-            self.P = None
-            return None
-        elif self.R is None:
-            print("Invalid Rotation matrix. Unable to compute P.")
-            self.P = None
-            return None
-        elif self.t is None:
-            print("Invalid translation vector. Unable to compute P.")
-            self.P = None
+            print("Error: unknown input matrix dimensions.")
             return None
 
-        RT = np.zeros((3, 4))
-        RT[:, 0:3] = self.R
-        RT[:, 3:4] = self.t
-        self.P = np.dot(self.K, RT)
-        return self.P
+        return block
 
-    def factor_P(self):
-        """Factorize the camera matrix into K, R, t as P = K[R | t]."""
-
-        # factor first 3*3 part
-        K, R = linalg.rq(self.P[:, :3])
-
-        # make diagonal of K positive
-        T = np.diag(np.sign(np.diag(K)))
-        if linalg.det(T) < 0:
-            T[1, 1] *= -1
-
-        self.K = np.dot(K, T)
-        self.R = np.dot(T, R)  # T is its own inverse
-        self.t = np.dot(linalg.inv(self.K), self.P[:, 3]).reshape(3, 1)
-
-        return self.K, self.R, self.t
-
-    def project_points(self, points3d):
+    def make_mat_homogeneous(self, mat) -> np.ndarray:
         """
-        Overhelmed method(see lib.geometry) for projecting 3D to image coordinates.
-
-        Project 3D points(Nx3 array) to image coordinates, given the projection matrix P(4x3 matrix)
-        If K matric and dist vector are given, the function computes undistorted image projections(otherwise, zero distortions are assumed)
-        Returns: 2D projected points(Nx2 array) in image coordinates
+        Return a homogeneous matrix, given a euclidean matrix (i.e., it adds a row of zeros with the last element as 1)
+            [     mat    ]
+            [------------]
+            [ 0  0  0  1 ]
         """
-        points3d = cv2.convertPointsToHomogeneous(points3d)[:, 0, :]
-        m = np.dot(self.P, points3d.T)
-        m = m[0:2, :] / m[2, :]
-        m = m.astype(float).T
+        n, m = mat.shape
+        mat_h = np.eye(n + 1, m)
+        mat_h[0:n, 0:m] = mat
 
-        if self.dist is not None and self.K is not None:
-            m = cv2.undistortPoints(m, self.K, self.dist, None, self.K)[:, 0, :]
-
-        return m.astype(float)
+        return mat_h
 
     def read_calibration_from_file(self, path: Union[str, Path]) -> None:
         """
@@ -313,45 +277,10 @@ class CameraNew:
         """
 
         w, h, K, dist = read_opencv_calibration(path)
-        self.width = w
-        self.height = h
-        self.K = K
-        self.dist = dist
-
-    def get_P_homogeneous(self):
-        """
-        Return the 4x4 P matrix from 3x4 P matrix, as:
-            [      P     ]
-            [------------]
-            [ 0  0  0  1 ]
-        """
-        P_hom = np.eye(4)
-        P_hom[0:3, 0:4] = self.P
-
-        return P_hom
-
-    def euler_from_R(self):
-        """
-        Compute Euler angles from rotation matrix
-        - ------
-        Returns:  [omega, phi, kappa]
-        """
-        omega = np.arctan2(self.R[2, 1], self.R[2, 2])
-        phi = np.arctan2(-self.R[2, 0], np.sqrt(self.R[2, 1] ** 2 + self.R[2, 2] ** 2))
-        kappa = np.arctan2(self.R[1, 0], self.R[0, 0])
-
-        return [omega, phi, kappa]
-
-    def build_block_matrix(self, mat):
-        if mat.shape[1] == 3:
-            block = np.block([[mat, np.zeros((3, 1))], [np.zeros((1, 3)), 1]])
-        elif mat.shape[1] == 1:
-            block = np.block([[np.eye(3), mat], [np.zeros((1, 3)), 1]])
-        else:
-            print("Error: unknown input matrix dimensions.")
-            return None
-
-        return block
+        self._width = w
+        self._height = h
+        self._K = K
+        self._dist = dist
 
 
 class Camera:
