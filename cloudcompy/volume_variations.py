b5d012d2478d4284cb67c21b1f7c88f18b05ac24
Francesco Ioli <francesco.ioli@polimi.it>
2023-01-16 21:08:42 +0100

    mesh interpolation and sampling

--- cloudcompy/volume_variations.py
+++ cloudcompy/volume_variations.py
@@ -9,18 +9,19 @@ import open3d as o3d
 
 from matplotlib import pyplot as plt
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Tuple, Union
 from multiprocessing import Pool, current_process
 
 from cloudcompy import DOD
 
-PCD_DIR = Path("res/point_clouds")  # Path("test")
+PCD_DIR = "cloudcompy/data"
 PCD_PATTERN = "dense*.ply"
 TSTEP = 3
 VERBOSE = True
 GRID_STEP = 0.2
-DIR = "x"
-FOUT = "cloudcompy/DOD_res_x_20cm.csv"  # "cloudcompy/DOD_res_x_20cm.csv"  #
+DOD_DIR = "x"
+OUT_DIR = "cloudcompy/res"
+FOUT = "DOD_res_x_20cm.csv"  # "cloudcompy/DOD_res_x_20cm.csv"  #
 
 
 def make_pairs(pcd_list: List[Path], step: int = 1) -> dict:
@@ -30,7 +31,7 @@ def make_pairs(pcd_list: List[Path], step: int = 1) -> dict:
     return pair_dict
 
 
-def iteration(
+def DOD_task(
     pcd_pair: Tuple,
 ):
 
@@ -40,87 +41,106 @@ def iteration(
     logging.info(f"Child {process.name} - Epoch {Path(pcd_pair[0]).stem} started")
 
     dod = DOD(pcd_pair)
-    dod.compute_volume(direction="x", grid_step=0.2)
-    dod.write_result_to_file(FOUT, mode="a+", header=False)
+    dod.compute_volume(direction=DOD_DIR, grid_step=GRID_STEP)
+    dod.write_result_to_file(Path(OUT_DIR) / FOUT, mode="a+", header=False)
     dod.clear()
     del dod
     gc.collect()
     logging.info(f"Child {process.name} completed.")
 
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+def meshing_task(
+    pcd_path: Union[str, Path],
+):
 
-assert PCD_DIR.is_dir(), "Directory does not exists."
-fout = Path(FOUT)
-if fout.exists():
-    logger.warning(f"Output file {fout} already exists. Removing it.")
-    fout.unlink()
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    process = current_process()
+    logging.info(f"Child {process.name} - Processing pcd {pcd_path}.")
 
-pcd_list = sorted(PCD_DIR.glob(PCD_PATTERN))
-pairs = make_pairs(pcd_list, TSTEP)
-pairs = [pair for pair in pairs.values()]
+    pcd = o3d.io.read_point_cloud(pcd_path)
+    assert Path(pcd).exists, "input point cloud does not exists."
 
-logging.info("Main process started.")
-t0 = time.time()
-pool = Pool()
-pool.map(iteration, pairs)
-pool.close()
-
-t1 = time.time()
-logging.info(f"DOD computation completed. Elapsed time: {t1-t0} sec")
+    logging.info(f"Child {process.name} completed.")
 
 
-# %% Read volume results from file and make plot
-df = DOD.read_results_from_file(FOUT)
-volumes = df.volume.to_numpy()
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
 
-fig, ax = plt.subplots(1, 1)
-ax.plot(volumes)
-ax.set_xlabel("day")
-ax.set_ylabel("m^3")
+    pcd_dir = Path(PCD_DIR)
+    out_dir = Path(OUT_DIR)
+    out_dir.mkdir(parents=True, exist_ok=True)
+    assert pcd_dir.is_dir(), f"Directory '{pcd_dir}' does not exists."
+    assert any(pcd_dir.iterdir()), f"Directory '{pcd_dir}' is empty."
 
-print(f"done")
+    fout = out_dir / FOUT
+    if fout.exists():
+        logger.warning(f"Output file {fout} already exists. Removing it.")
+        fout.unlink()
 
-#%% Single core processing
+    pcd_list = sorted(pcd_dir.glob(PCD_PATTERN))
+    pairs = make_pairs(pcd_list, TSTEP)
+    pairs = [pair for pair in pairs.values()]
 
-# Read point cloud list
-# assert PCD_DIR.is_dir(), "Directory does not exists."
-# pcd_list = sorted(PCD_DIR.glob(PCD_PATTERN))
-# pairs = make_pairs(pcd_list, TSTEP)
+    logging.info("DOD computation started:")
+    t0 = time.time()
+    pool = Pool()
+    pool.map(DOD_task, pairs)
+    pool.close()
+    t1 = time.time()
+    logging.info(f"DOD computation completed. Elapsed timMaine: {t1-t0:.2f} sec")
 
-# pcd_dict = {}
-# pcd_dict['epoch'] = pcd_list[0].stem[9:11]
-# pcd_dict['date_ground'] = pcd_list[0].stem[12:]
-# pcd_dict = dict.fromkeys(list(map(lambda path: path.stem[9:11], pcd_list)))
-# pcd_dict = {k,v for k,v in enumerate(pcd_list)}
+    # # %% Read volume results from file and make plot
+    # df = DOD.read_results_from_file(FOUT)
+    # volumes = df.volume.to_numpy()
 
-# Big Loop: Compute volumes
-# results = {}
-# for iter, pair in tqdm(pairs.items()):
-#     dod = DOD(pair)
-#     dod.compute_volume(direction=DIR, grid_step=GRID_STEP)
-#     dod.write_result_to_file(FOUT, mode="a+")
-#     results[iter] = dod.report.volume
-#     if VERBOSE:
-#         dod.print_result()
+    # fig, ax = plt.subplots(1, 1)
+    # ax.plot(volumes)
+    # ax.set_xlabel("day")
+    # ax.set_ylabel("m^3")
 
-#     # Free memory and clear pointers
-#     dod.clear()
-#     del dod
-#     gc.colrelect()
-#     if VERBOSE:
-#         print(
-#             f"Memory usage: {psutil.Process(os.getpid()).memory_info().rss / 1024**2:.1f} MB"
-#         )
-# print("done.")
+    # print(f"done")
 
-# # Plot
-# volumes = list(results.values())
-# fig, ax = plt.subplots(1, 1)
-# ax.plot(volumes)
-# ax.set_xlabel("epoch")
-# ax.set_ylabel("m^3")
-# ax.set_title(f"Volume difference with step of {TSTEP} days")
-# ax.grid(visible=True, color="k", linestyle="-", linewidth=0.2)
-# fig.savefig(Path(FOUT).parent / (Path(FOUT).stem + ".jpg"), dpi=300)
+    # # Single core processing
+
+    # Read point cloud list
+    # assert PCD_DIR.is_dir(), "Directory does not exists."
+    # pcd_list = sorted(PCD_DIR.glob(PCD_PATTERN))
+    # pairs = make_pairs(pcd_list, TSTEP)
+
+    # pcd_dict = {}
+    # pcd_dict['epoch'] = pcd_list[0].stem[9:11]
+    # pcd_dict['date_ground'] = pcd_list[0].stem[12:]
+    # pcd_dict = dict.fromkeys(list(map(lambda path: path.stem[9:11], pcd_list)))
+    # pcd_dict = {k,v for k,v in enumerate(pcd_list)}
+
+    # Big Loop: Compute volumes
+    # results = {}
+    # for iter, pair in tqdm(pairs.items()):
+    #     dod = DOD(pair)
+    #     dod.compute_volume(direction=DIR, grid_step=GRID_STEP)
+    #     dod.write_result_to_file(FOUT, mode="a+")
+    #     results[iter] = dod.report.volume
+    #     if VERBOSE:
+    #         dod.print_result()
+
+    #     # Free memory and clear pointers
+    #     dod.clear()
+    #     del dod
+    #     gc.colrelect()
+    #     if VERBOSE:
+    #         print(
+    #             f"Memory usage: {psutil.Process(os.getpid()).memory_info().rss / 1024**2:.1f} MB"
+    #         )
+    # print("done.")
+
+    # # Plot
+    # volumes = list(results.values())
+    # fig, ax = plt.subplots(1, 1)
+    # ax.plot(volumes)
+    # ax.set_xlabel("epoch")
+    # ax.set_ylabel("m^3")
+    # ax.set_title(f"Volume difference with step of {TSTEP} days")
+    # ax.grid(visible=True, color="k", linestyle="-", linewidth=0.2)
+    # fig.savefig(Path(FOUT).parent / (Path(FOUT).stem + ".jpg"), dpi=300)
