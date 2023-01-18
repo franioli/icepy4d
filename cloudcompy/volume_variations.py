00015e8a57d8ca289dc6550c0aa51282aadcf340
Francesco Ioli <francesco.ioli@polimi.it>
2023-01-18 22:16:22 +0100

    updated functions for meshing and computing DOD
    Former-commit-id: f0b7e946c7ac1d1b882fc15b541bd24604383ee5 [formerly f3f5a6aa3311983840851a7a4a826322512ed24f]
    Former-commit-id: 64f663f73fc6685618947e7a7618d6f29845d10c
    Former-commit-id: 7d5384ec19d55bbd1c4a1ab925aa610eb86c78f6
    Former-commit-id: 335693627712ace1388d95e2d9b659ef857ef19f
    Former-commit-id: 6dc599c0841905b7f851ca6af8b024a62a4f3e77

--- cloudcompy/volume_variations.py
+++ cloudcompy/volume_variations.py
@@ -11,24 +11,51 @@ from matplotlib import pyplot as plt
 from pathlib import Path
 from typing import List, Tuple, Union
 from multiprocessing import Pool, current_process
+from datetime import datetime, timedelta
 
 from cloudcompy import DOD
 
-PCD_DIR = "cloudcompy/data"
-PCD_PATTERN = "dense*.ply"
-TSTEP = 3
+PCD_DIR = "cloudcompy/meshed"  #"cloudcompy/data"  # 
+PCD_PATTERN = "sampled*.ply"  #"dense*.ply"  # 
+TSTEP = 5
 VERBOSE = True
-GRID_STEP = 0.2
+GRID_STEP = 0.5
 DOD_DIR = "x"
-OUT_DIR = "cloudcompy/res"
+OUT_DIR = "cloudcompy/dod_x"
 FOUT = "DOD_res_x_20cm.csv"  # "cloudcompy/DOD_res_x_20cm.csv"  #
 
+polyline_path = "cloudcompy/crop_polyline.poly"
 
-def make_pairs(pcd_list: List[Path], step: int = 1) -> dict:
+
+def find_closest_date_idx(
+    datetime_list: List[datetime],
+    date_to_find: datetime,
+):
+    closest = min(datetime_list, key=lambda sub: abs(sub - date_to_find))
+    for idx, date in enumerate(datetime_list):
+        if date == closest:
+            return idx
+
+
+def make_pairs(
+    pcd_list: List[Path], step: int = 1, date_format: str = "%Y_%m_%d"
+) -> dict:
+    # date_format = "%Y_%m_%d"
+    dt = timedelta(step)
+    idx = pcd_list[0].stem.find("202")
+    dates_str = [date.stem[idx:] for date in pcd_list]
+    dates = [datetime.strptime(date, date_format) for date in dates_str]
+
     pair_dict = {}
     for i in range(len(pcd_list) - step):
-        pair_dict[i] = (str(pcd_list[i]), str(pcd_list[i + step]))
-    return pair_dict
+        date_in = dates[i]
+        date_f = date_in + dt
+        idx_closest = find_closest_date_idx(dates, date_f)
+        pair_dict[i] = (str(pcd_list[i]), str(pcd_list[idx_closest]))
+
+        # pair_dict[i] = (str(pcd_list[i]), str(pcd_list[i + step]))
+
+    return (pair_dict, dates)
 
 
 def DOD_task(
@@ -41,6 +68,7 @@ def DOD_task(
     logging.info(f"Child {process.name} - Epoch {Path(pcd_pair[0]).stem} started")
 
     dod = DOD(pcd_pair)
+    # dod.cut_point_clouds_by_polyline(polyline_path, direction=DOD_DIR)
     dod.compute_volume(direction=DOD_DIR, grid_step=GRID_STEP)
     dod.write_result_to_file(Path(OUT_DIR) / FOUT, mode="a+", header=False)
     dod.clear()
@@ -49,24 +77,9 @@ def DOD_task(
     logging.info(f"Child {process.name} completed.")
 
 
-def meshing_task(
-    pcd_path: Union[str, Path],
-):
-
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    process = current_process()
-    logging.info(f"Child {process.name} - Processing pcd {pcd_path}.")
-
-    pcd = o3d.io.read_point_cloud(pcd_path)
-    assert Path(pcd).exists, "input point cloud does not exists."
-
-    logging.info(f"Child {process.name} completed.")
-
-
 if __name__ == "__main__":
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
 
     pcd_dir = Path(PCD_DIR)
     out_dir = Path(OUT_DIR)
@@ -80,8 +93,11 @@ if __name__ == "__main__":
         fout.unlink()
 
     pcd_list = sorted(pcd_dir.glob(PCD_PATTERN))
-    pairs = make_pairs(pcd_list, TSTEP)
+    pairs, dates = make_pairs(pcd_list, TSTEP)
     pairs = [pair for pair in pairs.values()]
+
+    # Test task
+    # DOD_task(pairs[0])
 
     logging.info("DOD computation started:")
     t0 = time.time()
@@ -91,16 +107,57 @@ if __name__ == "__main__":
     t1 = time.time()
     logging.info(f"DOD computation completed. Elapsed timMaine: {t1-t0:.2f} sec")
 
-    # # %% Read volume results from file and make plot
-    # df = DOD.read_results_from_file(FOUT)
-    # volumes = df.volume.to_numpy()
+    # Read volume results from file and make plot
+    column_names = [
+        "pcd0",
+        "pcd1",
+        "volume",
+        "addedVolume",
+        "removedVolume",
+        "surface",
+        "matchingPercent",
+        "averageNeighborsPerCell",
+    ]
+    df = pd.read_csv(fout, sep=",", names=column_names)
+    # df = DOD.read_results_from_file(fout, sep=',', column_names=column_names)
+    average_surface_match = df["matchingPercent"].to_numpy().mean()
+    df["date_in"] = pd.to_datetime(
+        df["pcd0"].str.replace(f"{PCD_PATTERN.split('*')[0]}_", ""), format="%Y_%m_%d"
+    )
+    df["date_fin"] = pd.to_datetime(
+        df["pcd1"].str.replace(f"{PCD_PATTERN.split('*')[0]}_", ""), format="%Y_%m_%d"
+    )
+    df["dt"] = (df.date_fin - df.date_in) / np.timedelta64(1, "D")
+    df["volume_daily"] = df["volume"] / df["dt"]
+    df["volume_daily_normalized"] = (
+        df["volume_daily"] * df["matchingPercent"] / average_surface_match
+    )
 
-    # fig, ax = plt.subplots(1, 1)
-    # ax.plot(volumes)
-    # ax.set_xlabel("day")
-    # ax.set_ylabel("m^3")
+    df.sort_values(by="date_in", inplace=True)
+    fname = f"{PCD_PATTERN.split('*')[0]}_step{TSTEP}_grid{GRID_STEP}.xlsx"
+    df.to_excel(out_dir / fname, index=False)
 
-    # print(f"done")
+    fig, ax = plt.subplots(1, 1)
+    ax.grid(visible=True, which="both")
+    ax.plot(df["date_in"], df["volume_daily"])
+    ax.set_xlabel("day")
+    ax.set_ylabel("m^3")
+    ax.set_title("Daily volume difference")
+    fig.set_size_inches(18.5, 10.5)
+    fname = f"{PCD_PATTERN.split('*')[0]}_step{TSTEP}_grid{GRID_STEP}.png"
+    fig.savefig(out_dir / fname, dpi=300)
+
+    fig, ax = plt.subplots(1, 1)
+    ax.grid(visible=True, which="both")
+    ax.plot(df["date_in"], df["volume_daily_normalized"])
+    ax.set_xlabel("day")
+    ax.set_ylabel("m^3")
+    ax.set_title("Daily volume difference")
+    fig.set_size_inches(18.5, 10.5)
+    fname = f"{PCD_PATTERN.split('*')[0]}_step{TSTEP}_grid{GRID_STEP}_normalized.png"
+    fig.savefig(out_dir / fname, dpi=300)
+
+    print(f"done")
 
     # # Single core processing
 
