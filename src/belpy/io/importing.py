697b2096d8238274918722f219d4af1c8228f1e5
Francesco Ioli <francesco.ioli@polimi.it>
2023-01-25 09:55:20 +0100

    simplified importing modules in main
    Former-commit-id: 839982592bd603f4772c90b35a29b32a266be366

--- src/belpy/io/importing.py
+++ src/belpy/io/importing.py
@@ -1,4 +1,3 @@
-import cv2
 import numpy as np
 import logging
 
@@ -45,40 +44,3 @@ def read_opencv_calibration(path: Union[str, Path], verbose: bool = False):
             )
 
     return w, h, K, dist
-
-
-""" Images """
-
-
-def process_resize(w, h, resize):
-    assert len(resize) > 0 and len(resize) <= 2
-    if len(resize) == 1 and resize[0] > -1:
-        scale = resize[0] / max(h, w)
-        w_new, h_new = int(round(w * scale)), int(round(h * scale))
-    elif len(resize) == 1 and resize[0] == -1:
-        w_new, h_new = w, h
-    else:  # len(resize) == 2:
-        w_new, h_new = resize[0], resize[1]
-
-    return w_new, h_new
-
-
-def read_img(path, color=True, resize=[-1], crop=None):
-    if color:
-        flag = cv2.IMREAD_COLOR
-    else:
-        flag = cv2.IMREAD_GRAYSCALE
-    image = cv2.imread(str(path), flag)
-
-    if image is None:
-        return None, None
-
-    w, h = image.shape[1], image.shape[0]
-    w_new, h_new = process_resize(w, h, resize)
-    scales = (float(w) / float(w_new), float(h) / float(h_new))
-    image = cv2.resize(image, (w_new, h_new))
-
-    if crop:
-        image = image[crop[1] : crop[3], crop[0] : crop[2]]
-
-    return image, scales
