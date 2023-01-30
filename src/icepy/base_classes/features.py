b1b32102b24d03c5fc07d2902a54f8ba33b899cf
Francesco Ioli <francesco.ioli@polimi.it>
2023-01-30 21:12:01 +0100

    implementing new Features class based on single Feature objects
    Former-commit-id: 844f2d7f872d8557baf3052b0ee4a9ee02025d07 [formerly 8e24d404a34a413c6a56696490dec57429da7ca1]
    Former-commit-id: f39b7e014e6ee043daa0dfc0225b4f132e9c7aa5
    Former-commit-id: d9165eece1b63b372f8e841769da9384b13b0248
    Former-commit-id: 862b330c852a8c87d6ed44284200e1d47532f1b7
    Former-commit-id: 6e39293c1f886dd841869d3167d2254e500bf379

--- src/icepy/base_classes/features.py
+++ src/icepy/base_classes/features.py
@@ -25,8 +25,177 @@ SOFTWARE.
 import pickle
 import numpy as np
 import logging
+import time
 
-from .camera import Camera
+from typing import Union, List
+
+if __name__ == "__main__":
+    from src.icepy.base_classes.camera import Camera
+else:
+    from .camera import Camera
+
+
+class Feature:
+    def __init__(
+        self,
+        x: Union[float, int],
+        y: Union[float, int],
+        track_id: int = None,
+        descr: np.ndarray = None,
+        score: float = None,
+    ) -> None:
+        """
+        __init__ Create Feature object
+
+        Args:
+            x (Union[float, int]): x coordinate (as for OpenCV image coordinate system)
+            y (Union[float, int]): y coordinate (as for OpenCV image coordinate system)
+            track_id (int, optional): track_id. Defaults to None.
+            descr (np.ndarray, optional): descriptor as a numpy array with 128 or 256 elements. Defaults to None.
+            score (float, optional): score. Defaults to None.
+
+        Raises:
+            AssertionError: Invalid shape of the descriptor. It must be a numpy array with 128 or 256 elements.
+        """
+        self._x = x
+        self._y = y
+
+        if track_id is not None:
+            assert isinstance(
+                track_id, int
+            ), "Invalid track_id. It must be a integer number"
+            self._track = track_id
+        else:
+            self._track = None
+
+        if descr is not None:
+            msg = "Invalid descriptor. It must be a numpy array with lenght of 128 or 256."
+            assert isinstance(descr, np.ndarray), msg
+            if len(descr.shape) == 1:
+                if descr.shape[0] not in [128, 256]:
+                    raise AssertionError(msg)
+                else:
+                    descr = descr.reshape(-1, 1)
+            elif descr.shape[1] not in [128, 256]:
+                raise AssertionError(msg)
+            self._descr = descr
+        else:
+            self._descr = None
+
+        if score is not None:
+            self._score = score
+        else:
+            self._score = None
+
+    @property
+    def x(self) -> float:
+        "Get x coordinate of the feature"
+        return float(self._x)
+
+    @property
+    def y(self) -> float:
+        "Get x coordinate of the feature"
+        return float(self._y)
+
+    @property
+    def xy(self) -> np.ndarray:
+        "Get xy coordinates of the feature as 1x2 numpy array"
+        return np.array([self._x, self._y], dtype=float).reshape(1, 2)
+
+    @property
+    def track_id(self) -> int:
+        "Get x coordinate of the feature"
+        if self._track is not None:
+            return int(self._track)
+        else:
+            logging.warning("Track id is not available")
+            return None
+
+    @property
+    def descr(self) -> np.ndarray:
+        """Get descriptor as mx1 numpy array (note that this is a column array)"""
+        if self._descr is not None:
+
+            return self._descr
+        else:
+            logging.warning("Descriptor is not available")
+            return None
+
+    @property
+    def score(self) -> float:
+        """Get score"""
+        if self._score is not None:
+            return float(self._score)
+        else:
+            logging.warning("Score is not available")
+            return None
+
+
+class Features_new:
+    def __init__(self):
+        self._values = {}
+        self._increm_id = 0
+
+    def __len__(self) -> int:
+        """
+        __len__ _summary_
+
+        Returns:
+            int: _description_
+        """
+        return len(self._values)
+
+    def __getitem__(self, track_id: int) -> str:
+        """
+        __getitem__ _summary_
+
+        Args:
+            track_id (int): _description_
+
+        Returns:
+            str: _description_
+        """
+        if track_id in list(self._values.keys()):
+            return self._values[track_id]
+        else:
+            logging.warning(f"Feature with track id {track_id} not available.")
+            return None
+
+    # def __iter__(self):
+    #     self._elem = 0
+    #     return self
+
+    # def __next__(self):
+    #     while self._elem < len(self):
+    #         file = self.files[self._elem]
+    #         self._elem += 1
+    #         return file
+    #     else:
+    #         self._elem
+    #         raise StopIteration
+
+    def append_feature(self, new_feature: Feature):
+        assert isinstance(
+            new_feature, Feature
+        ), "Invalid input feature. It must be Feature object"
+        self._values[self._increm_id] = new_feature
+        self._increm_id += 1
+
+    def append_features_from_numpy_array(self, x, y):
+        xx = x.flatten()
+        yy = y.flatten()
+        ids = range(self._increm_id, self._increm_id + len(xx))
+        for x, y, id in zip(xx, yy, ids):
+            self._values[id] = Feature(x, y)
+
+        self._increm_id = self._increm_id + len(xx)
+
+    def reset_fetures(self):
+        """
+        Reset Feature instance to None Objects
+        """
+        self._values = {}
+        self._increm_id = 0
 
 
 class Features:
@@ -37,10 +206,7 @@ class Features:
         Features.descr: mxn array of descriptors(note that descriptors are stored columnwise)
         Features.score: nx1 array with feature score"""
 
-    def __init__(
-        self,
-        logging: logging = None,
-    ):
+    def __init__(self):
         self.reset_fetures()
 
     def __len__(self):
@@ -153,4 +319,48 @@ class Features:
 
 if __name__ == "__main__":
     """Test classes"""
-    pass
+
+    from src.icepy.utils import setup_logger
+
+    setup_logger()
+
+    width, height = 6000, 4000
+    n_feat = 100000
+    x = np.random.randint(0, width, (n_feat, 1))
+    y = np.random.randint(0, height, (n_feat, 1))
+    kpts = np.concatenate((x, y), axis=1)
+    descr = np.random.rand(256, n_feat)
+    scores = np.random.rand(n_feat, 1)
+
+    t0 = time.time()
+    features = Features()
+    features.append_features(
+        {
+            "kpts": kpts,
+            "descr": descr,
+            "score": scores,
+        }
+    )
+    t1 = time.time()
+    logging.info(f"Append features as numpy array: elapsed time {t1-t0:.4f} s")
+
+    t0 = time.time()
+    features = Features_new()
+    for i in range(len(x)):
+        features.append_feature(Feature(x[i, 0], y[i, 0]))
+    t1 = time.time()
+    logging.info(
+        f"Append features as single Feature objects: Elapsed time {t1-t0:.4f} s"
+    )
+
+    t0 = time.time()
+    features = Features_new()
+    features.append_features_from_numpy_array(x, y)
+    t1 = time.time()
+    logging.info(
+        f"Append features from numpy array to dict of Feature objects: Elapsed time {t1-t0:.4f} s"
+    )
+
+    features[0].descr
+
+    print("Done")
