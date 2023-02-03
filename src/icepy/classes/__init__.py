e89959c9b2b7a0eb5fda777d63fac98fea1d9222
Francesco Ioli <francesco.ioli@polimi.it>
2023-02-03 17:22:37 +0100

    implementig getting features time series of tracked points

--- src/icepy/classes/__init__.py
+++ src/icepy/classes/__init__.py
@@ -1,5 +1,7 @@
 from .camera import Camera
 from .images import Image, ImageDS
-from .features import Features
+from .features import Feature, Features
 from .point_cloud import PointCloud
 from .targets import Targets
+from .point import Point
+from .typed_dict_classes import *
