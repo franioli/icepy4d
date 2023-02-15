42084483e3389cf841add584a727ab2aef5ef69b
Francesco Ioli <francesco.ioli@polimi.it>
2023-02-15 18:14:31 +0100

    implemented superglueMatcher class
    Former-commit-id: 5021ca1f554fdf62cc5a8d3eeb3423eb161f4e01

--- src/icepy/matching/superglue_matcher.py
+++ src/icepy/matching/superglue_matcher.py
@@ -11,6 +11,9 @@ Authors:
 import numpy as np
 import matplotlib.cm as cm
 import torch
+import logging
+import cv2
+import importlib
 
 from easydict import EasyDict as edict
 from pathlib import Path
@@ -22,46 +25,232 @@ from ..classes.images import ImageDS
 from ..utils.initialization import parse_yaml_cfg
 from ..tiles import generateTiles
 
-from ..thirdparty.SuperGluePretrainedNetwork.models.superpoint import SuperPoint
-from ..thirdparty.SuperGluePretrainedNetwork.models.superglue import SuperGlue
-from ..thirdparty.SuperGluePretrainedNetwork.models.utils import (
-    read_image,
-    frame2tensor,
+
+from icepy.matching.utils import read_image, frame2tensor
+from icepy.utils import AverageTimer
+
+
+from icepy.thirdparty.SuperGluePretrainedNetwork.models.superpoint import SuperPoint
+from icepy.thirdparty.SuperGluePretrainedNetwork.models.superglue import SuperGlue
+from icepy.thirdparty.SuperGluePretrainedNetwork.models.matching import Matching
+from icepy.thirdparty.SuperGluePretrainedNetwork.models.utils import (
+    make_matching_plot,
 )
 
 
-torch.set_grad_enabled(False)
-
-# SuperPoint constants
-# MODEL_WEIGHTS_PATH = Path(
-#     "thirdparty/SuperGluePretrainedNetwork/weights/superpoint_v1.pth")
+# SuperPoint Parameters
 NMS_RADIUS = 3
 
-
+# SuperGlue Parameters
 SUPERGLUE_DESC_DIM = 256
-
-# SuperGlue Hyperparameters
-DEFAULT_NUM_SINKHORN_ITERATIONS = 20
+SINKHORN_ITERATIONS = 20
 
 
-# class SuperPoint_(torch.nn.Module):
-#     """Image Matching Frontend (SuperPoint + SuperGlue)"""
+class SuperGlueMatcher:
+    def __init__(self, opt: dict) -> None:
+        """Initializes a SuperGlueMatcher object with the given options dictionary.
 
-#     def __init__(self, config={}):
-#         super().__init__()
-#         self.superpoint = SuperPoint(config.get("superpoint", {}))
+        The options dictionary should contain the following keys:
 
-#     def forward(self, data):
-#         """Run SuperPoint
-#         Args:
-#           data: dictionary with minimal keys: ['image']
-#         """
+        - 'superglue': path to the SuperGlue model weights file
+        - 'keypoint_threshold': threshold for the SuperPoint keypoint detector
+        - 'max_keypoints': maximum number of keypoints to extract with the SuperPoint detector
+        - 'match_threshold': threshold for the SuperGlue feature matcher
+        - 'force_cpu': whether to force using the CPU for inference
 
-#         # Extract SuperPoint (keypoints, scores, descriptors)
-#         pred = self.superpoint({"image": data["image"]})
-#         pred = {**pred, **{k + "0": v for k, v in pred.items()}}
+        Args:
+            opt (dict): a dictionary of options for configuring the SuperGlueMatcher object
 
-#         return pred
+        Raises:
+            KeyError: if one or more required options are missing from the options dictionary
+            FileNotFoundError: if the specified SuperGlue model weights file cannot be found
+        """
+        # @TODO: check options input
+        self.opt = edict(opt)
+
+        self.features0 = None
+        self.features1 = None
+        self.matches0 = None
+        self.matches1 = None
+        self.match_conf = None
+        self.mkpts0 = None
+        self.mkpts1 = None
+
+        # Temporary (@TODO to be changed globally)
+        if "superglue" in self.opt.keys():
+            self.opt.weights = self.opt["superglue"]
+            del self.opt["superglue"]
+
+        self.device = (
+            "cuda" if torch.cuda.is_available() and not self.opt.force_cpu else "cpu"
+        )
+        logging.info(f"Running inference on device {self.device}")
+
+        self.config = {
+            "superpoint": {
+                "nms_radius": NMS_RADIUS,
+                "keypoint_threshold": self.opt.keypoint_threshold,
+                "max_keypoints": self.opt.max_keypoints,
+            },
+            "superglue": {
+                "weights": self.opt.weights,
+                "sinkhorn_iterations": SINKHORN_ITERATIONS,
+                "match_threshold": self.opt.match_threshold,
+            },
+        }
+
+    def match(self, image0: np.ndarray, image1: np.ndarray):
+        """Matches keypoints and descriptors in two images using the SuperGlue algorithm.
+
+        This method takes in two images as Numpy arrays, and returns the matches between keypoints
+        and descriptors in those images using the SuperGlue algorithm.
+
+        Args:
+            image0 (np.ndarray): the first image to match, as Numpy array
+            image1 (np.ndarray): the second image to match, as Numpy array
+
+        Returns:
+            None
+        """
+
+        if len(image0.shape) > 2:
+            image0 = cv2.cvtColor(image0, cv2.COLOR_RGB2GRAY)
+        if len(image1.shape) > 2:
+            image1 = cv2.cvtColor(image1, cv2.COLOR_RGB2GRAY)
+
+        tensor0 = frame2tensor(image0, self.device)
+        tensor1 = frame2tensor(image1, self.device)
+
+        self.timer = AverageTimer()
+        matching = Matching(self.config).eval().to(self.device)
+        self.pred = matching({"image0": tensor0, "image1": tensor1})
+        self.timer.update("matching")
+        unpack = {k: v[0].cpu().numpy() for k, v in self.pred.items()}
+
+        self.features0 = edict(
+            {
+                "keypoints": unpack["keypoints0"],
+                "descriptors": unpack["descriptors0"],
+                "scores": unpack["scores0"],
+            }
+        )
+        self.features1 = edict(
+            {
+                "keypoints": unpack["keypoints1"],
+                "descriptors": unpack["descriptors1"],
+                "scores": unpack["scores1"],
+            }
+        )
+        self.matches0 = unpack["matches0"]
+        self.matches1 = unpack["matches1"]
+
+        valid_idx_0 = self.matches0 > -1
+        valid_idx_1 = self.matches0[valid_idx_0]
+        self.mkpts0 = self.features0.keypoints[valid_idx_0]
+        self.mkpts1 = self.features1.keypoints[valid_idx_1]
+        self.descriptors0 = self.features0.descriptors[:, valid_idx_0]
+        self.descriptors1 = self.features1.descriptors[:, valid_idx_1]
+        self.scores0 = self.features0.scores[valid_idx_0]
+        self.scores1 = self.features1.scores[valid_idx_1]
+        self.match_conf = unpack["matching_scores0"][valid_idx_0]
+
+        self.timer.print("SuperGlue matching")
+
+    def geometric_verification(
+        self,
+        threshold: float = 1,
+        confidence: float = 0.9999,
+        max_iters: int = 10000,
+        laf_consistensy_coef: float = -1.0,
+        error_type: str = "sampson",
+        symmetric_error_check: bool = True,
+        enable_degeneracy_check: bool = True,
+    ) -> np.ndarray:
+        assert self.mkpts0 is not None, "Matches not available."
+
+        try:
+            pydegensac = importlib.import_module("pydegensac")
+            self.F, self.inlMask = pydegensac.findFundamentalMatrix(
+                self.mkpts0,
+                self.mkpts1,
+                px_th=threshold,
+                conf=confidence,
+                max_iters=max_iters,
+                laf_consistensy_coef=laf_consistensy_coef,
+                error_type=error_type,
+                symmetric_error_check=symmetric_error_check,
+                enable_degeneracy_check=enable_degeneracy_check,
+            )
+            logging.info(
+                f"Pydegensac found {self.inlMask.sum()} inliers ({self.inlMask.sum()*100/len(self.mkpts0):.2f}%)"
+            )
+        except:
+            logging.error(
+                "Pydegensac not available. Using MAGSAC++ (OpenCV) for geometric verification."
+            )
+            self.F, inliers = cv2.findFundamentalMat(
+                self.mkpts0, self.mkpts1, cv2.USAC_MAGSAC, 0.5, 0.999, 100000
+            )
+            self.inlMask = inliers > 0
+            logging.info(
+                f"Pydegensac found {self.inlMask.sum()} inliers ({self.inlMask.sum()*100/len(self.mkpts0):.2f}%)"
+            )
+
+        self.mkpts0 = self.mkpts0[self.inlMask]
+        self.mkpts1 = self.mkpts1[self.inlMask]
+
+        return self.inlMask
+
+    def viz_matches(
+        self,
+        image0: np.ndarray,
+        image1: np.ndarray,
+        path: str,
+        fast_viz: bool = False,
+        show_keypoints: bool = False,
+        opencv_display: bool = False,
+    ) -> None:
+        assert self.mkpts0 is not None, "Matches not available."
+        # image0 = np.uint8(tensor0.cpu().numpy() * 255),
+
+        if fast_viz and len(image0.shape) > 2:
+            image0 = cv2.cvtColor(image0, cv2.COLOR_RGB2GRAY)
+        if fast_viz and len(image1.shape) > 2:
+            image1 = cv2.cvtColor(image1, cv2.COLOR_RGB2GRAY)
+
+        color = cm.jet(self.match_conf)
+        text = [
+            "SuperGlue",
+            "Keypoints: {}:{}".format(
+                len(self.features0.keypoints), len(self.features1.keypoints)
+            ),
+            "Matches: {}".format(len(self.mkpts0)),
+        ]
+
+        # Display extra parameter info.
+        k_thresh = self.opt["keypoint_threshold"]
+        m_thresh = self.opt["match_threshold"]
+        small_text = [
+            "Keypoint Threshold: {:.4f}".format(k_thresh),
+            "Match Threshold: {:.2f}".format(m_thresh),
+        ]
+
+        make_matching_plot(
+            image0,
+            image1,
+            self.mkpts0,
+            self.mkpts1,
+            self.mkpts0,
+            self.mkpts1,
+            color,
+            text,
+            path=path,
+            show_keypoints=show_keypoints,
+            fast_viz=fast_viz,
+            opencv_display=opencv_display,
+            opencv_title="Matches",
+            small_text=small_text,
+        )
 
 
 class SuperPoint_features:
@@ -118,80 +307,6 @@ class SuperPoint_features:
         # )
 
         # return features
-
-
-# class SuperGlue_matcher():
-#     """Implements the SuperGlue matcher -- a pretrained graph neural network using attention."""
-
-#     def __init__(self,
-#                  #  image0: np.ndarray,
-#                  #  image1: np.ndarray,
-#                  use_cuda: bool = True,
-#                  use_outdoor_model: bool = True,
-#                  #  max_keypoints: int = 2048,
-#                  #  keypoint_threshold: float = 0.0001,
-#                  match_threshold: float = 0.15,
-#                  config: edict = None,
-#                  ) -> None:
-#         """Initialize the configuration and the parameters."""
-
-#         self._config = {
-#             'superpoint': {
-#                 'nms_radius': NMS_RADIUS,
-#                 'keypoint_threshold': keypoint_threshold,
-#                 'max_keypoints': max_keypoints
-#             },
-#             'superglue': {
-#                 'weights': "outdoor" if use_outdoor_model else "indoor",
-#                 'sinkhorn_iterations': DEFAULT_NUM_SINKHORN_ITERATIONS,
-#                 'match_threshold': match_threshold,
-#             }
-#         }
-
-#         self._use_cuda = use_cuda and torch.cuda.is_available()
-
-#     def detect_and_match(self,
-#                          max_keypoints: int = 5000,
-#                          use_cuda: bool = True,
-#                          weights_path: Path = MODEL_WEIGHTS_PATH,
-#                          use_cudaopt: edict):
-#         """ Run Superpoint for finding features and match them with SuperGlue
-
-#         Args:
-#             max_keypoints: max keypoints to detect in an image.
-#             use_cuda (optional): flag controlling the use of GPUs via CUDA. Defaults to True.
-#             weights_path (optional): Path to the model weights. Defaults to MODEL_WEIGHT_PATH.
-#         """
-
-#         device = torch.device("cuda" if self._use_cuda else "cpu")
-#         model = SuperPoint(self._config).to(device)
-#         model.eval()
-
-#         # Compute features.
-#         image_tensor = torch.from_numpy(
-#             np.expand_dims(image_utils.rgb_to_gray_cv(
-#                 image).value_array.astype(np.float32) / 255.0, (0, 1))
-#         ).to(device)
-#         with torch.no_grad():
-#             model_results = model({"image": image_tensor})
-#         torch.cuda.empty_cache()
-
-#         # Unpack results.
-#         coordinates = model_results["keypoints"][0].detach().cpu().numpy()
-#         scores = model_results["scores"][0].detach().cpu().numpy()
-#         keypoints = Keypoints(coordinates, scales=None, responses=scores)
-#         descriptors = model_results["descriptors"][0].detach().cpu().numpy().T
-
-#         # Filter features.
-#         if image.mask is not None:
-#             keypoints, valid_idxs = keypoints.filter_by_mask(image.mask)
-#             descriptors = descriptors[valid_idxs]
-#         keypoints, selection_idxs = keypoints.get_top_k(self.max_keypoints)
-#         descriptors = descriptors[selection_idxs]
-
-#         return keypoints, descriptors
-
-#         print('')
 
 
 if __name__ == "__main__":
