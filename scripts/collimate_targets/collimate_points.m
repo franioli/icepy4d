a8839e05a3275d0111ff1f76101866b6fad2a678
Francesco Ioli <francesco.ioli@polimi.it>
2023-03-01 11:45:14 +0100

    moved all scripts to scripts folder

---
+++ scripts/collimate_targets/collimate_points.m
@@ -0,0 +1,61 @@
+function [varargout] = collimate_points(im, varargin)
+%collimatePoints
+%  points = collimate_points(im, npts=1, axLims=axLims, printPoints=false);
+% 
+% To do:
+% - add labels to points
+% - add points in input
+
+%% Prase input
+defnpts = Inf;
+defprintsOut = false;
+defAxLim = {};
+
+p = inputParser;
+addRequired(p,'im',@isnumeric);
+addParameter(p,'npts',defnpts,@isnumeric)
+addParameter(p,'printPoints',defprintsOut,@islogical)
+addParameter(p,'axLims',defAxLim)
+parse(p,im,varargin{:})
+
+npts  = p.Results.npts;
+printPoints  = p.Results.printPoints;
+axLims = p.Results.axLims;
+
+%%
+p = struct();
+fig = figure("Name",'Press Enter to confirm the position of collimated point', 'Units','normalized', 'Position',[0 0 1 1]); 
+ax = axes('Parent',fig);
+imshow(im); 
+hold on; axis on;
+
+if ~isempty(axLims)
+    xlim(axLims{1})
+    ylim(axLims{2})    
+end
+
+ii = 1;
+while ishghandle(fig) && ii <= npts
+    roi = drawpoint(ax);
+    pause;
+    p(ii).loc = roi.Position;
+    clearvars roi
+    ii = ii + 1;
+end
+close(fig)
+
+points = zeros(length(p),2);
+for jj = 1:length(p)
+    points(jj,:) = [p(jj).loc];
+end
+
+if printPoints
+    figure("Name",'Collimated points', 'Units','normalized', 'Position',[0 0 1 1]); 
+    imshow(im); hold on;
+    plot(points(:,1), points(:,2), 'gx');
+end
+
+if nargout == 1
+    varargout = {points};
+
+end
