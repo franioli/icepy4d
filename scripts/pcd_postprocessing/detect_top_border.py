22a975603ca82c721253bdad04a645cdac063c64
Francesco Ioli <francesco.ioli@polimi.it>
2023-03-29 09:13:55 +0200

    updated detect_top_border and result processing
    Former-commit-id: 5d9e2db64b9475759fd8e430f9e3e139068d9de8 [formerly 0ba6ee2c96f8f693a5ed1457ab90f027451275c7]
    Former-commit-id: efe7d7be86e434e8188bbae467fcb9689a4f59e4
    Former-commit-id: 163a2d870376be16bbb4df549cea4de1a0ffaf1b
    Former-commit-id: 01c2ca5c74dcb9dfd6696634e4f475f2409cf63b
    Former-commit-id: c2dc27036ec3c02509d6878013b1b3f93ea8acbf

--- scripts/pcd_postprocessing/detect_top_border.py
+++ scripts/pcd_postprocessing/detect_top_border.py
@@ -118,9 +118,9 @@ def detect_border_by_geometry():
         if not export_coordinates_as_scalar_fields(pcd_filtered):
             raise RuntimeError(f"Unable to export coordinates as scalar")
 
-        path = str(output_dir / ("filtered_" + pcd_date))
-        if not cc.SavePointCloud(pcd_filtered, path):
-            raise IOError(f"Unable to save cropped point cloud to {path}.")
+        # path = str(output_dir / ("filtered_" + pcd_date + ".ply"))
+        # if not cc.SavePointCloud(pcd_filtered, path):
+        #     raise IOError(f"Unable to save cropped point cloud to {path}.")
 
         lim_percentile = (60, 95)
         z_coord_sf = pcd_filtered.toNpArrayCopy()[:, 2]
@@ -130,7 +130,7 @@ def detect_border_by_geometry():
         pcd_filtered.setCurrentOutScalarField(sf_num)
         pcd_border = cc.filterBySFValue(sf_min, sf_max, pcd_filtered)
 
-        path = str(output_dir / ("border_" + pcd_date))
+        path = str(output_dir / ("border_" + pcd_date + ".ply"))
         if not cc.SavePointCloud(pcd_border, path):
             raise IOError(f"Unable to save cropped point cloud to {path}.")
 
@@ -145,45 +145,58 @@ def extract_glacier_border():
     output_dir.mkdir(exist_ok=True)
     base_name = PCD_PATTERN.split("_")[0]
 
+    logging.info("Computing border median coordinates...")
     with open(output_dir / FOUT_NAME, "w") as f:
         f.write(
             f"pcd_name,date,x_mean,x_median,x_std,y_mean,y_median,y_std,z_mean,z_median,z_std\n"
         )
         for pcd_path in pcd_list:
             logging.info(f"Processing pcd {pcd_path.name}")
+            try:
+                pcd_date = pcd_path.stem.replace(base_name + "_", "")
+                pcd_border = cc.loadPointCloud(str(pcd_path))
+                if pcd_border is None:
+                    raise IOError(f"Unable to read point cloud {pcd_path}")
 
-            pcd_date = pcd_path.stem.replace(base_name + "_", "")
-            pcd_border = cc.loadPointCloud(str(pcd_path))
-            if pcd_border is None:
-                raise IOError(f"Unable to read point cloud {pcd_path}")
+                ylims = (222.0, 228.0)
+                try:
+                    sf_num = pcd_border.getScalarFieldDic()["Coord. Y"]
+                except:
+                    raise RuntimeError("Unable to get Coord. Y scalar field")
+                pcd_border.setCurrentOutScalarField(sf_num)
+                pcd_border = cc.filterBySFValue(ylims[0], ylims[1], pcd_border)
 
-            ylims = (222.0, 228.0)
-            sf_num = pcd_border.getScalarFieldDic()["Coord. Y"]
-            pcd_border.setCurrentOutScalarField(sf_num)
-            pcd_border = cc.filterBySFValue(ylims[0], ylims[1], pcd_border)
+                x_coord_sf = pcd_border.toNpArrayCopy()[:, 0]
+                median_x = np.median(x_coord_sf)
+                sf_min = median_x - 10
+                sf_max = median_x + 10
+                try:
+                    sf_num = pcd_border.getScalarFieldDic()["Coord. X"]
+                except:
+                    raise RuntimeError("Unable to get Coord. X scalar field")
+                pcd_border.setCurrentOutScalarField(sf_num)
+                pcd_border = cc.filterBySFValue(sf_min, sf_max, pcd_border)
 
-            x_coord_sf = pcd_border.toNpArrayCopy()[:, 0]
-            median_x = np.median(x_coord_sf)
-            sf_min = median_x - 10
-            sf_max = median_x + 10
-            sf_num = pcd_border.getScalarFieldDic()["Coord. X"]
-            pcd_border.setCurrentOutScalarField(sf_num)
-            pcd_border = cc.filterBySFValue(sf_min, sf_max, pcd_border)
+                coords = pcd_border.toNpArrayCopy()
+                mean = np.mean(coords, axis=0)
+                median = np.median(coords, axis=0)
+                std = np.std(coords, axis=0)
 
-            coords = pcd_border.toNpArrayCopy()
-            mean = np.mean(coords, axis=0)
-            median = np.median(coords, axis=0)
-            std = np.std(coords, axis=0)
-
-            f.write(
-                f"{pcd_path.name},{pcd_date},{mean[0]:.3f},{median[0]:.3f},{std[0]:.3f},{mean[1]:.3f},{median[1]:.3f},{std[1]:.3f},{mean[2]:.3f},{median[2]:.3f},{std[2]:.3f}\n"
-            )
+                f.write(
+                    f"{pcd_path.name},{pcd_date},{mean[0]:.3f},{median[0]:.3f},{std[0]:.3f},{mean[1]:.3f},{median[1]:.3f},{std[1]:.3f},{mean[2]:.3f},{median[2]:.3f},{std[2]:.3f}\n"
+                )
+            except RuntimeError as err:
+                logging.exception(err)
+                f.write(
+                    f"{pcd_path.name},{pcd_date},NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN\n"
+                )
+                logging.info("Moving to next pcd.")
             cc.deleteEntity(pcd_border)
 
 
 if __name__ == "__main__":
 
-    detect_border_by_geometry()
+    # detect_border_by_geometry()
     extract_glacier_border()
 
     print("done.")
