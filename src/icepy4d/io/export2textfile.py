f77eaa9b26e1bf7acaa3c0b4986e8df963c07ae6
Francesco Ioli <francesco.ioli@polimi.it>
2023-08-04 13:05:26 +0200

    cleaned and refractored main code
    Former-commit-id: b9da3ab0163f65b58bfe49fbb3202a2fd424cb75

--- src/icepy4d/io/export2textfile.py
+++ src/icepy4d/io/export2textfile.py
@@ -2,6 +2,7 @@ import numpy as np
 import logging
 from pathlib import Path
 from typing import Union
+import pandas as pd
 
 from icepy4d.thirdparty.transformations import euler_from_matrix
 from icepy4d.classes.epoch import Epoch
@@ -9,7 +10,7 @@ from icepy4d.classes.features import Features
 from icepy4d.classes.images import ImageDS
 
 
-def write_cameras_to_disk(
+def write_cameras_to_file(
     output_path: Union[Path, str], epoch: Epoch, sep: str = ","
 ) -> None:
     """Write camera parameters for a given epoch to a CSV file.
@@ -53,6 +54,70 @@ def write_cameras_to_disk(
             o, p, k = np.rad2deg(o), np.rad2deg(p), np.rad2deg(k)
             file.write(f"{sep}{f:.2f}{sep}{o:.4f}{sep}{p:.4f}{sep}{k:.4f}")
         file.write("\n")
+
+
+def write_reprojection_error_to_file(
+    output_path: Union[Path, str], epoch: Epoch, sep: str = ","
+) -> None:
+    """Write reprojection error statistics to a CSV file.
+
+    This function computes and writes the reprojection error statistics for
+    each camera in the epoch to a CSV file. The reprojection error is the
+    difference between the projected 3D points and their corresponding 2D
+    keypoints in each camera. The statistics include the mean, standard
+    deviation, minimum, 25th percentile, median, 75th percentile, and maximum
+    of the reprojection errors for each camera and a global norm computed as
+    the mean of all cameras' reprojection errors.
+
+    Args:
+        output_path (Union[Path, str]): The path to the output CSV file.
+        epoch (Epoch): The Epoch object containing camera parameters and
+        keypoints.
+        sep (str, optional): The separator to use in the CSV file. Defaults to
+        ",".
+
+    Returns:
+        None: The function does not return anything. The statistics are written
+        to the CSV file.
+
+    """
+
+    cams = list(epoch.cameras.keys())
+    output_path = Path(output_path)
+
+    residuals = pd.DataFrame()
+    for cam_key, camera in epoch.cameras.items():
+        feat = epoch.features[cam_key]
+        projections = camera.project_point(epoch.points.to_numpy())
+        res = projections - feat.kpts_to_numpy()
+        res_norm = np.linalg.norm(res, axis=1)
+        residuals["track_id"] = feat.get_track_ids()
+        residuals[f"x_{cam_key}"] = res[:, 0]
+        residuals[f"y_{cam_key}"] = res[:, 1]
+        residuals[f"norm_{cam_key}"] = res_norm
+
+    # Compute global norm as mean of all cameras
+    residuals["global_norm"] = np.mean(
+        residuals[[f"norm_{x}" for x in cams]].to_numpy(), axis=1
+    )
+    res_stas = residuals.describe()
+    res_stas_s = res_stas.stack()
+
+    if not output_path.exists():
+        with open(output_path, "w") as f:
+            header_line = (
+                "ep"
+                + sep
+                + f"{sep}".join([f"{x[0]}-{x[1]}" for x in res_stas_s.index.to_list()])
+            )
+            f.write(header_line + "\n")
+    with open(output_path, "a") as f:
+        line = (
+            str(epoch.timestamp)
+            + sep
+            + f"{sep}".join([str(x) for x in res_stas_s.to_list()])
+        )
+        f.write(line + "\n")
 
 
 """
